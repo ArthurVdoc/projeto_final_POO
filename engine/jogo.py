c46de290d5df9c4e5dbabe81747ffb86ce32ff3d
ArthurVdoc <arthurviniciusocart@gmail.com>
2025-06-09 10:18:48 -0300

    atualizado funções de execução e passadas para a classe jogo. Atualizado main.py, deixando-a enxuta

---
+++ engine/jogo.py
@@ -0,0 +1,48 @@
+from entidades.mapa import Mapa
+from menus.menu_1 import Menu_1
+import constantes
+
+import pygame
+
+class Jogo:
+    def __init__(self):
+        #inicia o pygame
+        pygame.init()
+        #cria a tela
+        self.tela = pygame.display.set_mode((constantes.LARGURA_TELA, constantes.ALTURA_TELA))
+        pygame.display.set_caption("Projeto Final POO")
+        #cria o clock temporal
+        self.clock = pygame.time.Clock()
+        #cria o mapa
+        self.mapa = Mapa()
+
+        self.menu_1_ativo = None
+        self.jogo_ativo = True
+    
+    def executar(self):
+        while self.jogo_ativo:
+            self.tratar_eventos()
+            self.desenhar()
+            pygame.display.update()
+            self.clock.tick(30)
+        pygame.quit()
+
+    def tratar_eventos(self):
+        for evento in pygame.event.get():
+            if evento.type == pygame.QUIT:
+                self.jogo_ativo = False
+            elif self.menu_1_ativo:
+                self.menu_1_ativo.tratar_evento(evento)
+            elif evento.type == pygame.MOUSEBUTTONDOWN:
+                bloco = self.mapa.obter_bloco(evento.pos[0], evento.pos[1])
+                if bloco:
+                    self.menu_1_ativo = Menu_1(bloco, self.fechar_menu)
+    
+    def fechar_menu(self):
+        self.menu_1_ativo = None
+
+    def desenhar(self):
+        self.tela.fill(constantes.AZUL)
+        self.mapa.desenhar(self.tela)
+        if self.menu_1_ativo:
+            self.menu_1_ativo.desenhar(self.tela)
--- main.py
+++ main.py
@@ -1,35 +1,8 @@
-import pygame
-from mapa import Mapa
-import constantes
+from engine.jogo import Jogo
 
-pygame.init()
+def main():
+    jogo = Jogo()
+    jogo.executar
 
-#Tela
-tela = pygame.display.set_mode((constantes.LARGURA_TELA, constantes.ALTURA_TELA))
-pygame.display.set_caption("Projeto Final POO")
-
-clock = pygame.time.Clock()
-
-#Cria o objeto Mapa
-mapa = Mapa()
-
-#Loop para o jogo
-jogo_aberto = True
-while jogo_aberto:
-    for evento in pygame.event.get():
-        if evento.type == pygame.QUIT:
-            jogo_aberto = False
-        elif evento.type == pygame.MOUSEBUTTONDOWN:
-            posição = pygame.mouse.get_pos()
-            cord_x = posição[0]
-            cord_y = posição[1]
-            mapa.processar_click(cord_x, cord_y)
-
-    tela.fill(constantes.AZUL)
-    mapa.desenhar(tela)
-    pygame.display.update()
-    clock.tick(30)
-
-pygame.quit()
-
-
+if __name__ == "__main__":
+    main()
