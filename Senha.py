dee92b03737001a8a67565e7330d14b5e33c319d
Guilherme Garcia <garcia.guilherme3006@gmail.com>
2025-06-23 11:17:13 -0300

    Alterar senha é mais seguro

--- Senha.py
+++ Senha.py
@@ -17,7 +17,7 @@ janela.geometry("400x500")
 
 # Função: alterar visualização da senha 1
 def alternar_senha1():
-    entrada_novasenha.configure(show="" if check_senha1.get() else "*")
+    entrada_senha.configure(show="" if check_senha1.get() else "*")
 
 # Função: alterar visualização da senha 2
 def alternar_senha2():
@@ -25,27 +25,26 @@ def alternar_senha2():
 
 # Função: alterar senha
 def alterar_senha():
+    usuarios = Login.extrair()
     usuario = entrada_usuario.get().strip()
-    senha = entrada_novasenha.get()
+    senha = entrada_senha.get()
     confirmarsenha = confirmar_novasenha.get()
 
     if not usuario or not senha or not confirmarsenha:
         messagebox.showwarning("Erro", "Preencha todos os campos.")
         return
-
-    if senha != confirmarsenha:
-        messagebox.showerror("Erro", "As senhas não coincidem.")
-        return
-
-    usuarios = Login.extrair()
+    
     if usuario not in usuarios:
         messagebox.showerror("Erro", f"Usuário '{usuario}' não encontrado.")
-        return
-
-    Login.alterar(usuario, senha)
-    messagebox.showinfo("Alterar senha", f"Senha de '{usuario}' alterada com sucesso!")
-    janela.quit()
-    subprocess.Popen(["python", "Login.py"])
+    else:
+        if senha == usuarios[usuario]:
+            Login.alterar(usuario, confirmarsenha)
+            messagebox.showinfo("Alterar senha", f"Senha de '{usuario}' alterada com sucesso!")
+            janela.quit()
+            subprocess.Popen(["python", "Login.py"])
+        
+        else:
+            messagebox.showwarning("Erro", f"Senha incompátivel com o {usuario}")
 
 def voltar():
     janela.quit()
@@ -55,9 +54,9 @@ def voltar():
 entrada_usuario = ctk.CTkEntry(janela, placeholder_text="Usuário")
 entrada_usuario.pack(pady=10)
 
-# Campo nova senha
-entrada_novasenha = ctk.CTkEntry(janela, placeholder_text="Insira a nova senha", show="*")
-entrada_novasenha.pack(pady=5)
+# Campo senha
+entrada_senha = ctk.CTkEntry(janela, placeholder_text="Insira senha atual", show="*")
+entrada_senha.pack(pady=5)
 
 # Checkbox senha 1
 check_senha1 = ctk.BooleanVar()
@@ -65,7 +64,7 @@ mostrar_checkbox1 = ctk.CTkCheckBox(janela, text="Mostrar senha", variable=check
 mostrar_checkbox1.pack()
 
 # Campo confirmar senha
-confirmar_novasenha = ctk.CTkEntry(janela, placeholder_text="Digite novamente a nova senha", show="*")
+confirmar_novasenha = ctk.CTkEntry(janela, placeholder_text="Digite a nova senha", show="*")
 confirmar_novasenha.pack(pady=5)
 
 # Checkbox senha 2
